0e9bed33ba319cb468fc79b0a06ffb483c048aa2
jackun <jack.un@gmail.com>
2016-04-25 19:00:48 +0300

    vl: move function bodies to header. MSVC can't cope with it otherwise and is probably more correct anyway from c++ viewpoint.

--- src/qemu-usb/vl.cpp
+++ src/qemu-usb/vl.cpp
@@ -1,19 +1,5 @@
 #include "vl.h"
 
-void cpu_physical_memory_rw(uint32_t addr, uint8_t *buf,
-                            int len, int is_write);
-
-inline void cpu_physical_memory_read(uint32_t addr, 
-                                            uint8_t *buf, int len)
-{
-    cpu_physical_memory_rw(addr, buf, len, 0);
-}
-inline void cpu_physical_memory_write(uint32_t addr, 
-                                             const uint8_t *buf, int len)
-{
-    cpu_physical_memory_rw(addr, (uint8_t *)buf, len, 1);
-}
-
 /* compute with 96 bit intermediate result: (a*b)/c */
 uint64_t muldiv64(uint64_t a, uint32_t b, uint32_t c)
 {
