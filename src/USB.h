d00e893e17c7d15795202bf7d3053bff0f804ff4
Jack Un <jack.un@gmail.com>
2016-05-13 22:00:53 +0300

    Put OSDebugOut to separate header.
    Don't have to pull in stuff from USB.h

--- src/USB.h
+++ src/USB.h
@@ -39,60 +39,15 @@
 #include <windows.h>
 #include <windowsx.h>
 
-#include <vector>
-static int rateLimit = 0;
-static void _OSDebugOut(const TCHAR *psz_fmt, ...)
-{
-	if(rateLimit > 0 && rateLimit < 100)
-	{
-		rateLimit++;
-		return;
-	}
-	else
-	{
-		//rateLimit = 1;
-	}
-
-	va_list args;
-	va_start(args, psz_fmt);
-
-#ifdef UNICODE
-	int bufsize = _vscwprintf(psz_fmt, args) + 1;
-	std::vector<TCHAR> msg(bufsize);
-	vswprintf_s(&msg[0], bufsize, psz_fmt, args);
-#else
-	int bufsize = _vscprintf(psz_fmt, args) + 1;
-	std::vector<TCHAR> msg(bufsize);
-	vsprintf_s(&msg[0], bufsize, psz_fmt, args);
-#endif
-
-	//_vsnwprintf_s(&msg[0], bufsize, bufsize-1, psz_fmt, args);
-	va_end(args);
-
-	OutputDebugString(&msg[0]);
-}
-
-#ifdef _DEBUG
-//Too many gibberish intellisense errors
-//#define OSDebugOut(psz_fmt, ...) _OSDebugOut( TEXT("[") TEXT(__FUNCTION__) TEXT("] ") psz_fmt, ##__VA_ARGS__)
-#define OSDebugOut(psz_fmt, ...) _OSDebugOut(psz_fmt, ##__VA_ARGS__)
-#else
-#define OSDebugOut(...) do{}while(0)
-#endif
-
 #else //_WIN32
 
 //#include <gtk/gtk.h>
 #include <limits.h>
 #include <string>
 
-#ifdef _DEBUG
-#define OSDebugOut(psz_fmt, ...) fprintf(stderr, psz_fmt, ##__VA_ARGS__)
-#else
-#define OSDebugOut(psz_fmt, ...)
-#endif
-
 #endif //_WIN32
+
+#include "osdebugout.h"
 
 // ---------------------------------------------------------------------
 #define USBdefs
