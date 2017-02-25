ab34dad87c60a8e0e5437becaa2ac0e756842fe0
jackun <jack.un@gmail.com>
2017-02-25 21:44:49 +0200

    win32: wasapi settings, remove dlg init data

--- src/Win32/resource.h
+++ src/Win32/resource.h
@@ -8,13 +8,14 @@
 #define IDD_CONFIG                      101
 #define IDD_ABOUT                       103
 #define IDD_DLGMSD                      106
-#define IDD_DLGMIC                      107
+#define IDD_DLGWASAPI                   107
 #define IDC_NAME                        1000
 #define IDC_LOGGING                     1007
 #define IDC_COMBO1                      1008
 #define IDC_COMBO2                      1009
 #define IDC_LIST1                       1010
 #define IDC_COMBO_API1                  1010
+#define IDC_COMBO3                      1010
 #define IDC_COMBO_FFB                   1011
 #define IDC_COMBO_API2                  1011
 #define IDC_BUTTON1                     1012
@@ -23,19 +24,17 @@
 #define IDC_EDIT1                       1015
 #define IDC_COMBO_WHEEL_TYPE1           1037
 #define IDC_COMBO_WHEEL_TYPE2           1038
-#define IDC_COMBOMIC1                   1040
-#define IDC_COMBOMIC2                   1041
-#define IDC_MICSLIDER                   1042
-#define IDC_MICBUF                      1043
-#define IDC_COMBOMICAPI                 1044
+#define IDC_SLIDER1                     1039
+#define IDC_BUFFER1                     1040
+#define IDC_COMBOMICAPI                 1041
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1044
+#define _APS_NEXT_CONTROL_VALUE         1042
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
