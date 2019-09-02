0c44a69cade4ef11c4bd40bdfc49005591afd9f2
jackun <jack.un@gmail.com>
2019-09-02 18:47:03 +0300

    linux/util: use stat

--- src/linux/util.cpp
+++ src/linux/util.cpp
@@ -1,25 +1,16 @@
 #include "util.h"
-#include <dirent.h>
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 bool file_exists(std::string path)
 {
-	FILE *i = fopen(path.c_str(), "r");
-
-	if (i == NULL)
-		return false;
-
-	fclose(i);
-	return true;
+	struct stat s;
+	return !stat(path.c_str(), &s) && !S_ISDIR(s.st_mode);
 }
 
 bool dir_exists(std::string path)
 {
-	DIR *i = opendir(path.c_str());
-
-	if (i == NULL)
-		return false;
-
-	closedir(i);
-	return true;
+	struct stat s;
+	return !stat(path.c_str(), &s) && S_ISDIR(s.st_mode);
 }
