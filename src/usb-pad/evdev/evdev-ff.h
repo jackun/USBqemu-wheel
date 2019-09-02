9d50b3c1f5bfeab078767db2a6322fd515254cef
jackun <jack.un@gmail.com>
2019-09-02 18:49:14 +0300

    usb-pad/evdev: add more effects, maybe they even work with some wheels.

--- src/usb-pad/evdev/evdev-ff.h
+++ src/usb-pad/evdev/evdev-ff.h
@@ -6,6 +6,14 @@
 
 namespace usb_pad { namespace evdev {
 
+enum EffectID
+{
+	EFF_CONSTANT = 0,
+	EFF_SPRING,
+	EFF_DAMPER,
+	EFF_FRICTION,
+};
+
 class EvdevFF
 {
 public:
@@ -13,17 +21,20 @@ public:
 	~EvdevFF();
 
 	void SetConstantForce(int force);
-	void SetSpringForce(int force);
+	void SetSpringForce(const spring& force, int caps);
+	void SetDamperForce(const damper& force, int caps);
+	void SetFrictionForce(const friction& frict);
 	void SetAutoCenter(int value);
 	void SetGain(int gain);
-	void DisableConstantForce();
+	void DisableForce(EffectID force);
 	void TokenOut(ff_data *ffdata, bool hires);
 
 private:
 	int mHandle;
-	ff_effect mEffConstant;
+	ff_effect mEffect;
 	ff_effect mEffRumble;
 	ff_state mFFstate;
+	int mEffIds[3] = {-1, -1, -1}; //save ids just in case
 
 	bool mUseRumble;
 	int mLastValue;
