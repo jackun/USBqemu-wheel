3dd160aaae3915ef9f1679e4b851ccd75f1d3150
jackun <jack.un@gmail.com>
2019-12-02 11:15:42 +0200

    usb-hid: add no-op api

--- src/usb-hid/api_init_win32.cpp
+++ src/usb-hid/api_init_win32.cpp
@@ -1,8 +1,10 @@
 #include "hidproxy.h"
 #include "raw/rawinput.h"
+#include "noop.h"
 
 void usb_hid::RegisterUsbHID::Initialize()
 {
 	auto& inst = RegisterUsbHID::instance();
 	inst.Add(usb_hid::raw::APINAME, new UsbHIDProxy<usb_hid::raw::RawInput>());
+	inst.Add(usb_hid::noop::APINAME, new UsbHIDProxy<usb_hid::noop::NOOP>());
 }
