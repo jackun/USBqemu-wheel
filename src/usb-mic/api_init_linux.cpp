188ed349dbf27a37e4fd6e577c822952b5415606
jackun <jack.un@gmail.com>
2020-01-14 11:46:37 +0200

    usb-mic/noop: namespace, cleanup

--- src/usb-mic/api_init_linux.cpp
+++ src/usb-mic/api_init_linux.cpp
@@ -5,6 +5,6 @@
 void RegisterAudioDevice::Initialize()
 {
 	auto& inst = RegisterAudioDevice::instance();
-	inst.Add(audiodev_noop::APINAME, new AudioDeviceProxy<audiodev_noop::NoopAudioDevice>());
+	inst.Add(usb_mic::audiodev_noop::APINAME, new AudioDeviceProxy<usb_mic::audiodev_noop::NoopAudioDevice>());
 	inst.Add(audiodev_pulse::APINAME, new AudioDeviceProxy<audiodev_pulse::PulseAudioDevice>());
 }
